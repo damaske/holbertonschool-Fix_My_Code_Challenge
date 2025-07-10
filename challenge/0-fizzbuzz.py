cd55c32a8e56c41faf22756ef9927de4f882dec6
damaske <damaske.vzk@gmail.com>
2025-07-10 00:31:41 +0000

    some

---
+++ challenge/0-fizzbuzz.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" FizzBuzz
+"""
+import sys
+
+
+def fizzbuzz(n):
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+
+    - For multiples of both 3 and 5 print "FizzBuzz".
+    - For multiples of three print "Fizz".
+    - For multiples of five print "Buzz".
+    - Otherwise, print the number.
+    """
+    if n < 1:
+        return
+
+    tmp_result = []
+    for i in range(1, n + 1):
+        if (i % 3 == 0) and (i % 5 == 0):
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    try:
+        number = int(sys.argv[1])
+        fizzbuzz(number)
+    except ValueError:
+        print("Invalid number")
+        sys.exit(1)
